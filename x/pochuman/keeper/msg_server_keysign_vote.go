214cbfa2f757f79ec46a9623259b31f928da7b90
VigorousDeveloper <dev.mania.639@gmail.com>
2022-06-29 23:53:07 +0700

    implement keysign vote

--- x/pochuman/keeper/msg_server_keysign_vote.go
+++ x/pochuman/keeper/msg_server_keysign_vote.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/VigorousDeveloper/poc-human/x/pochuman/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -10,8 +11,24 @@ import (
 func (k msgServer) KeysignVote(goCtx context.Context, msg *types.MsgKeysignVote) (*types.MsgKeysignVoteResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	//
+	n := k.GetKeysignTxStoreCount(ctx)
+	index := fmt.Sprintf("%d", n+1)
 
-	return &types.MsgKeysignVoteResponse{}, nil
+	// Create a new instance of ObserveVote
+	transaction := types.KeysignVoteData{
+		Index:  index,
+		TxHash: msg.TxHash,
+		PubKey: msg.PubKey,
+		Voter:  msg.Creator,
+		TxTime: ctx.BlockTime().UTC().String(),
+	}
+
+	// Add new keysign data
+	k.SetKeysignVoteData(ctx, transaction)
+
+	// Update Requested Vote Count
+	k.SetKeysignTxStoreCount(ctx, n+1)
+
+	return &types.MsgKeysignVoteResponse{Code: "200", Msg: "Operation succeed"}, nil
 }
