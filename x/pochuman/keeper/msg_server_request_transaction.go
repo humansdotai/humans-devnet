0d19489605c55edf22a2367d00dd1e1e5380e24d
VigorousDeveloper <dev.mania.639@gmail.com>
2022-06-29 23:50:54 +0700

    implement request transaction tx

--- x/pochuman/keeper/msg_server_request_transaction.go
+++ x/pochuman/keeper/msg_server_request_transaction.go
@@ -2,16 +2,121 @@ package keeper
 
 import (
 	"context"
+	"fmt"
+	"strconv"
 
 	"github.com/VigorousDeveloper/poc-human/x/pochuman/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) RequestTransaction(goCtx context.Context, msg *types.MsgRequestTransaction) (*types.MsgRequestTransactionResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	amt, err := strconv.ParseFloat(msg.Amount, 64)
+	if err != nil || amt <= 0 {
+		return &types.MsgRequestTransactionResponse{Code: "501", Msg: "Invalid amount parameter"}, nil
+	}
 
-	return &types.MsgRequestTransactionResponse{}, nil
+	if msg.OriginChain != types.CHAIN_ETHEREUM && msg.OriginChain != types.CHAIN_POLYGON && msg.OriginChain != types.CHAIN_SOLANA {
+		return &types.MsgRequestTransactionResponse{Code: "501", Msg: "Invalid chain parameter"}, sdkerrors.Wrapf(sdkerrors.ErrInvalidCoins, "invalid chain")
+	}
+
+	if msg.TargetChain != types.CHAIN_ETHEREUM && msg.TargetChain != types.CHAIN_POLYGON && msg.TargetChain != types.CHAIN_SOLANA {
+		return &types.MsgRequestTransactionResponse{Code: "501", Msg: "Invalid chain parameter"}, sdkerrors.Wrapf(sdkerrors.ErrInvalidCoins, "invalid chain")
+	}
+
+	// check destination chain
+	if msg.TargetChain == types.CHAIN_SOLANA {
+		balance, er := k.GetPoolBalance(ctx, "2")
+		if !er {
+			return &types.MsgRequestTransactionResponse{Code: "501", Msg: "Insufficient balance"}, sdkerrors.Wrap(sdkerrors.ErrInsufficientFunds, "Solana pool doens't have enough balance")
+		}
+
+		b, e := strconv.ParseFloat(balance.Balance, 64)
+		if e != nil {
+			return &types.MsgRequestTransactionResponse{Code: "501", Msg: "Insufficient balance"}, sdkerrors.Wrap(sdkerrors.ErrInsufficientFunds, "Solana pool doens't have enough balance")
+		}
+
+		// if pool balance is lower than transfer amount
+		if b < amt {
+			return &types.MsgRequestTransactionResponse{Code: "501", Msg: "Insufficient balance"}, sdkerrors.Wrap(sdkerrors.ErrInsufficientFunds, "Solana pool doens't have enough balance")
+		}
+	}
+
+	// check dest chain
+	if msg.TargetChain == types.CHAIN_ETHEREUM {
+		balance, er := k.GetPoolBalance(ctx, "1")
+		if !er {
+			return &types.MsgRequestTransactionResponse{Code: "501", Msg: "Insufficient balance"}, sdkerrors.Wrap(sdkerrors.ErrInsufficientFunds, "Ethereum pool doens't have enough balance")
+		}
+
+		b, e := strconv.ParseFloat(balance.Balance, 64)
+		if e != nil {
+			return &types.MsgRequestTransactionResponse{Code: "501", Msg: "Insufficient balance"}, sdkerrors.Wrap(sdkerrors.ErrInsufficientFunds, "Ethereum pool doens't have enough balance")
+		}
+
+		// if pool balance is lower than transfer amount
+		if b < amt {
+			return &types.MsgRequestTransactionResponse{Code: "501", Msg: "Insufficient balance"}, sdkerrors.Wrap(sdkerrors.ErrInsufficientFunds, "Ethereum pool doens't have enough balance")
+		}
+	}
+
+	// check dest chain
+	if msg.TargetChain == types.CHAIN_POLYGON {
+		balance, er := k.GetPoolBalance(ctx, "3")
+		if !er {
+			return &types.MsgRequestTransactionResponse{Code: "501", Msg: "Insufficient balance"}, sdkerrors.Wrap(sdkerrors.ErrInsufficientFunds, "Polygon pool doens't have enough balance")
+		}
+
+		b, e := strconv.ParseFloat(balance.Balance, 64)
+		if e != nil {
+			return &types.MsgRequestTransactionResponse{Code: "501", Msg: "Insufficient balance"}, sdkerrors.Wrap(sdkerrors.ErrInsufficientFunds, "Polygon pool doens't have enough balance")
+		}
+
+		// if pool balance is lower than transfer amount
+		if b < amt {
+			return &types.MsgRequestTransactionResponse{Code: "501", Msg: "Insufficient balance"}, sdkerrors.Wrap(sdkerrors.ErrInsufficientFunds, "Polygon pool doens't have enough balance")
+		}
+	}
+
+	// Convert player address strings to sdk.AccAddress
+	payer, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+	}
+
+	// take service fee from the payer
+	fee := sdk.Coins{sdk.NewInt64Coin("uKMA", 1e9)}
+	err = k.bankKeeper.SendCoinsFromAccountToModule(ctx, payer, types.ModuleName, fee)
+	if err != nil {
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidCoins, "invalid coins (%s)", err)
+	}
+
+	n := k.GetReqTransactionStoreCount(ctx)
+	index := fmt.Sprintf("%d", n+1)
+
+	// Create a new instance of Bet
+	transaction := types.TransactionData{
+		Index:              index,
+		OriginChain:        msg.OriginChain,
+		TargetChain:        msg.TargetChain,
+		Amount:             msg.Amount,
+		Time:               ctx.BlockTime().UTC().String(),
+		Creator:            msg.Creator,
+		OriginAddress:      msg.OriginAddress,
+		TargetAddress:      msg.TargetAddress,
+		ConfirmedBlockHash: "",
+		Status:             types.PAY_AVAILABLE,
+		SignedKey:          "",
+		Fee:                msg.Fee,
+	}
+
+	// Add new transaction data
+	k.SetTransactionData(ctx, transaction)
+
+	// Update Requested Transaction Count
+	k.SetReqTransactionStoreCount(ctx, n+1)
+
+	return &types.MsgRequestTransactionResponse{Code: "200", Msg: "Operation succeed"}, nil
 }
