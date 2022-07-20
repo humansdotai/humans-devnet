75cfe45ace321196bdf901f640209ce1e2061c02
Jason07250000 <venus07250000@gmail.com>
2022-07-20 17:30:49 +0800

    customize mint module

---
+++ x/mint/keeper/integration_test.go
@@ -0,0 +1,72 @@
+package keeper_test
+
+import (
+	"encoding/json"
+
+	"github.com/ignite-hq/cli/ignite/pkg/cosmoscmd"
+	abci "github.com/tendermint/tendermint/abci/types"
+	"github.com/tendermint/tendermint/libs/log"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+	dbm "github.com/tendermint/tm-db"
+
+	junoapp "github.com/VigorousDeveloper/poc-human/app"
+	"github.com/cosmos/cosmos-sdk/simapp"
+
+	"github.com/VigorousDeveloper/poc-human/x/mint/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// returns context and an app with updated mint keeper
+func createTestApp(isCheckTx bool) (*junoapp.App, sdk.Context) {
+	app := setup(isCheckTx)
+
+	ctx := app.BaseApp.NewContext(isCheckTx, tmproto.Header{})
+	app.MintKeeper.SetParams(ctx, types.DefaultParams())
+	app.MintKeeper.SetMinter(ctx, types.DefaultInitialMinter())
+
+	return app, ctx
+}
+
+func setup(isCheckTx bool) *junoapp.App {
+	app, genesisState := genApp(!isCheckTx, 5)
+	if !isCheckTx {
+		// init chain must be called to stop deliverState from being nil
+		stateBytes, err := json.MarshalIndent(genesisState, "", " ")
+		if err != nil {
+			panic(err)
+		}
+
+		// Initialize the chain
+		app.InitChain(
+			abci.RequestInitChain{
+				Validators:      []abci.ValidatorUpdate{},
+				ConsensusParams: simapp.DefaultConsensusParams,
+				AppStateBytes:   stateBytes,
+			},
+		)
+	}
+
+	return app
+}
+
+func genApp(withGenesis bool, invCheckPeriod uint) (*junoapp.App, junoapp.GenesisState) {
+	db := dbm.NewMemDB()
+	encCdc := cosmoscmd.MakeEncodingConfig(junoapp.ModuleBasics)
+	app := junoapp.New(
+		log.NewNopLogger(),
+		db,
+		nil,
+		true,
+		map[int64]bool{},
+		simapp.DefaultNodeHome,
+		invCheckPeriod,
+		encCdc,
+		simapp.EmptyAppOptions{})
+
+	originalApp := app.(*junoapp.App)
+	if withGenesis {
+		return originalApp, junoapp.NewDefaultGenesisState(encCdc.Marshaler)
+	}
+
+	return originalApp, junoapp.GenesisState{}
+}
